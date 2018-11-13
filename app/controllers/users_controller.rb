f4a5af10df728d641f82c06c5b5eca81d0c555ad
jimboqu <jamesm.quilter@gmail.com>
2018-11-13 14:30:41 +0000

    Set up Clearance to accept username or email on login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < Clearance::UsersController
 
+  def new
+    @user = User.new
+    render template: "users/new"
+  end
 
   private
     def user_params
       params.require(:user).permit(:email, :username, :password)
     end
+
 
 end
